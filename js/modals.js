0a6aa77877670dec9bf6156f833925ff99af26b5
Marcel Balint <blank_blankness@yahoo.com>
2019-05-08 10:34:48 +0000

    Created the map modal functionality

--- js/modals.js
+++ js/modals.js
@@ -5,19 +5,35 @@ var floatingPanel = document.querySelector('#floatingPanel');
 var locationField = document.querySelector('#locationField');
 var closeBtn = document.querySelector('.close');
 
+
 contactBtn.addEventListener('click', function() {
     modalContainer.style.display = "block";
-     modalContainer.style.zIndex = "5";
-     floatingPanel.style.zIndex = "1";
-     locationField.style.zIndex = "1";
+    modalContainer.style.zIndex = "5";
+    floatingPanel.style.zIndex = "1";
+    locationField.style.zIndex = "1";
 });
 
 closeBtn.addEventListener('click', function() {
-     modalContainer.style.display = "none";
+    modalContainer.style.display = "none";
 });
 
 window.onclick = function(event) {
-    if(event.target == modalContainer) {
+    if (event.target == modalContainer) {
         modalContainer.style.display = "none";
     }
 };
+
+// Map modal
+
+var main = document.getElementById("main");
+
+function removeModal() {
+    main.parentNode.removeChild(main);
+}
+
+function appendModal() {
+    var body = document.getElementById("body");
+    body.appendChild(main);
+}
+
+window.addEventListener('load', removeModal);
