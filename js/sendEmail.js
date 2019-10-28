33b42c9f579e7073906a4994d86b145c44a4a816
Marcel Balint <blank_blankness@yahoo.com>
2019-10-28 07:45:31 -0400

    Modified the form submission

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,19 +1,18 @@
 var emailjs;
 
 function sendMail(contactForm) {
-    emailjs.send("gmail", "marcel_balint", {
-            "from_name": contactForm.name.value,
-            "from_email": contactForm.email.value,
-            "project": contactForm.comment.value
-            
-        })
-        .then(
-            function(response) {
-                console.log("SUCCESS", response);
-            },
-            function(error) {
-                console.log("FAILED", error);
-            }
-        );
+  emailjs
+    .send("gmail", "marcel_balint", {
+      from_name: contactForm.name.value,
+      from_email: contactForm.email.value,
+      project: contactForm.comment.value
+    })
+    .then(
+      function(response) {
+        console.log("SUCCESS", response);
+      },
+      function(error) {
+        console.log("FAILED", error);
+      }
+    );
 }
-
